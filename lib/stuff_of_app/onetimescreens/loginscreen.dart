283cc4906030b78d4d36ac13e06fdb12820a67e6
LUNAGOK <21lunagok@gmail.com>
2023-01-29 20:17:29 +0530

    002

--- lib/stuff_of_app/onetimescreens/loginscreen.dart
+++ lib/stuff_of_app/onetimescreens/loginscreen.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
